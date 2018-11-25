5290cef9e03630ba41c04debfa21cdc410fc4b63
LyashenkoValeria <leovaldez00@mai.ru>
2018-11-26 00:30:56 +0300

    lesson 8

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -2,10 +2,7 @@
 package lesson8.task1
 
 import lesson1.task1.sqr
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import kotlin.math.*
 
 /**
  * Точка на плоскости
@@ -154,7 +151,12 @@ class Line private constructor(val b: Double, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val x = (other.b * cos(angle) - b * cos(other.angle)) / (sin(angle - other.angle))
+        val y = if (abs(cos(angle)) > (cos(other.angle))) x * tan(angle) + b / cos(angle)
+        else x * tan(other.angle) + other.b / cos(other.angle)
+        return Point(x, y)
+    }
 
     override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
 
